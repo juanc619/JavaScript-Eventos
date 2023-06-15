7192f9ec8d59966f14e62224bd70bbc876702beb
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-14 20:59:38 -0500

    cambiar modo color

--- js/main.js
+++ js/main.js
@@ -10,8 +10,24 @@ function mostrarAlerta() {
     alert("hola mundo");
 }
 
-// Mostrar mensaje en la consola
+// Mostrar mensaje en la consola -- escribir una funcion anonima
 const consola = document.querySelector("#consola");
 consola.addEventListener("click", () => {
     console.log("Hola mundo");
 })
+
+// Agregar clase a un elemento -- switch color mode
+const colorModeButton = document.querySelector("#color-mode");
+const body = document.body;
+
+colorModeButton.addEventListener("click", cambiarModoColor);
+
+function cambiarModoColor() {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        colorModeButton.innerText = "Cambiar a Light Mode";
+    } else {
+        colorModeButton.innerText = "Cambiar a Dark Mode";
+    }
+}
